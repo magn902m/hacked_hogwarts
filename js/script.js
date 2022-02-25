49b1b06cda0ee61ebd447afb7871858eeef21d2f
Magnus <magn902m@stud.kea.dk>
2022-02-25 10:31:27 +0100

    fixed cleanup and added searchbar in html

--- js/script.js
+++ js/script.js
@@ -24,6 +24,7 @@ const settings = {
   filterBy: "all",
   sortBy: "name",
   sortDir: "asc",
+  direction: 1,
 };
 
 const HTML = {};
@@ -34,7 +35,6 @@ function setup() {
   const urlList = "https://petlatkea.dk/2021/hogwarts/students.json";
   const urlBlood = "https://petlatkea.dk/2021/hogwarts/families.json";
 
-  // cleanUpData(getData(allStudents));
   getData(urlList, urlBlood);
 }
 
@@ -46,94 +46,105 @@ async function getData(urlList, urlBlood) {
   const jsonDataBlood = await responseBlood.json();
   // console.log(jsonData);
   // return allStudents;
-  convertJsonData(jsonDataList, jsonDataBlood);
+  preparecleanUp(jsonDataList, jsonDataBlood);
 }
 
-function convertJsonData(jsonDataList, jsonDataBlood) {
+function preparecleanUp(jsonDataList, jsonDataBlood) {
   const studentsList = jsonDataList;
   const bloodStatus = jsonDataBlood;
-  cleanUpData(studentsList, bloodStatus);
+  console.log(bloodStatus);
+  allStudents = studentsList.map(cleanUpData);
+  displayList(allStudents);
 }
 
 function cleanUpData(studentsList, bloodStatus) {
   // console.log(studentsList, bloodStatus);
 
-  // create a object form the prototype
-  studentsList.forEach((elm) => {
-    const student = Object.create(Student);
+  const student = Object.create(Student);
 
-    // Variable for holding data and trim elm for whitespace
-    let house = elm.house.trim();
-    let gender = elm.gender.trim();
-    let originalName = elm.fullname.trim();
+  // Variable for holding data and trim elm for whitespace
+  let house = studentsList.house.trim();
+  let gender = studentsList.gender.trim();
+  let originalName = studentsList.fullname.trim();
 
-    // -- Setting values for the object --
-    // Firstname: take the first char and set it to upper case and set the rest to lower case.
+  // -- Setting values for the object --
+  // Firstname: take the first char and set it to upper case and set the rest to lower case.
+  if (originalName.includes(" ")) {
     student.firstName =
       originalName.substring(0, 1).toUpperCase() +
       originalName.substring(1, originalName.indexOf(" ")).toLowerCase();
+  } else {
+    student.firstName =
+      originalName.substring(0, 1).toUpperCase() + originalName.substring(1).toLowerCase();
+  }
 
-    // Lastname: take the first char in the lastname and make it upper case and the rest lower case.
+  // Lastname: take the first char in the lastname and make it upper case and the rest lower case.
+  if (originalName.includes(" ")) {
     student.lastName =
       originalName
         .substring(originalName.lastIndexOf(" ") + 1, originalName.lastIndexOf(" ") + 2)
         .toUpperCase() + originalName.substring(originalName.lastIndexOf(" ") + 2).toLowerCase();
+  }
 
-    // Middlename: take the middlename and make the first char upper case and the rest lower case.
-    student.middleName =
-      originalName
-        .substring(originalName.indexOf(" "), originalName.lastIndexOf(" "))
-        .trim()
-        .substring(0, 1)
-        .toUpperCase() +
-      originalName
-        .substring(originalName.indexOf(" "), originalName.lastIndexOf(" "))
-        .trim()
-        .substring(1)
-        .toLowerCase();
+  // if (originalName.includes("-")) {
+  //   student.lastName = originalName.substring(originalName.indexOf("-")).toUpperCase();
+  // }
 
-    // Nickname: find the nickname with "" in a if statement.
-    if (originalName.includes(`"`)) {
-      student.nickName = originalName.substring(
-        originalName.indexOf(`"`) + 1,
-        originalName.lastIndexOf(`"`)
-      );
-      student.middleName = "";
-    }
+  // Middlename: take the middlename and make the first char upper case and the rest lower case.
 
-    // Gender: first char set to upper case, rest to lower case.
-    student.gender = gender.charAt(0).toUpperCase() + gender.substring(1).toLowerCase();
+  student.middleName = originalName.substring(
+    originalName.indexOf(" ") + 1,
+    originalName.lastIndexOf(" ")
+  );
+  student.middleName =
+    student.middleName.substring(0, 1).toUpperCase() +
+    student.middleName.substring(1).toLowerCase();
 
-    // Imgsrc: find the destation and make it all to lower case.
-    student.imgSrc = `./assets/images/${originalName
-      .substring(0, originalName.indexOf(" "))
-      .toLowerCase()}_.png`;
-    student.imgSrc = `./assets/images/${
-      originalName
-        .substring(originalName.lastIndexOf(" ") + 1, originalName.lastIndexOf(" ") + 2)
-        .toLowerCase() + originalName.substring(originalName.lastIndexOf(" ") + 2).toLowerCase()
-    }_${originalName.substring(0, 1).toUpperCase().toLowerCase()}.png`;
+  // Nickname: find the nickname with "" in a if statement.
+  if (originalName.includes(`"`)) {
+    student.middleName = "";
+    student.nickName = originalName.substring(
+      originalName.indexOf(`"`),
+      originalName.lastIndexOf(`"`) + 1
+    );
+  }
 
-    // House: set the first char to upper case and the rest to lower case.
-    student.house = house.charAt(0).toUpperCase() + house.substring(1).toLowerCase();
+  // Gender: first char set to upper case, rest to lower case.
+  student.gender = gender.charAt(0).toUpperCase() + gender.substring(1).toLowerCase();
 
-    allStudents.push(student);
-    displayStudent(student);
-  });
-  showAllStudents();
+  // Imgsrc: find the destation and make it all to lower case.
+  student.imgSrc = `./assets/images/${originalName
+    .substring(0, originalName.indexOf(" "))
+    .toLowerCase()}_.png`;
+  student.imgSrc = `./assets/images/${
+    originalName
+      .substring(originalName.lastIndexOf(" ") + 1, originalName.lastIndexOf(" ") + 2)
+      .toLowerCase() + originalName.substring(originalName.lastIndexOf(" ") + 2).toLowerCase()
+  }_${originalName.substring(0, 1).toUpperCase().toLowerCase()}.png`;
+
+  // House: set the first char to upper case and the rest to lower case.
+  student.house = house.charAt(0).toUpperCase() + house.substring(1).toLowerCase();
+
+  return student;
 }
 
-function showAllStudents() {
-  console.table(allStudents);
+function displayList(students) {
+  console.table(students);
+  // clear the list
+  document.querySelector("#student_list tbody").innerHTML = ";";
+  // build a new list
+  students.forEach(displayStudent);
 }
 
 function displayStudent(student) {
   const clone = document.querySelector("template#student").content.cloneNode(true);
 
-  clone.querySelector("[data-field=firstname]").textContent =
-    student.firstName + " " + student.nickName + " " + student.middleName;
+  clone.querySelector(
+    "[data-field=firstname]"
+  ).textContent = `${student.firstName} ${student.nickName} ${student.middleName}`;
   clone.querySelector("[data-field=lastname]").textContent = student.lastName;
   clone.querySelector("[data-field=house]").textContent = student.house;
+  clone.querySelector("[data-field=prefect]").textContent = "🎖";
   //   clone.querySelector(".popup_button").addEventListener("click", openPopup);
   document.querySelector("tbody").appendChild(clone);
 }
